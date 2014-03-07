b2acbdf0156de382c05b34cb1a02a0867ab64b27
Jun Chen <junc76@gmail.com>
2014-03-07 17:22:23 +0800

    Create a crash. In progress!

--- Dump/Dump.cpp
+++ Dump/Dump.cpp
@@ -4,7 +4,25 @@
 #include "stdafx.h"
 #include "stdio.h"
 #include "minidump.h"
+#include <DbgHelp.h>
+
+#pragma comment( lib, "DbgHelp" )
+
+LONG WINAPI MyUnhandledExceptionFilter(struct _EXCEPTION_POINTERS* ExceptionInfo)
+{
+	HANDLE lhDumpFile = CreateFile(L"test.dmp", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	MINIDUMP_EXCEPTION_INFORMATION loExceptionInfo;
+	loExceptionInfo.ExceptionPointers = ExceptionInfo;
+	loExceptionInfo.ThreadId = GetCurrentThreadId();
+	loExceptionInfo.ClientPointers = TRUE;
+	MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), lhDumpFile, MiniDumpNormal, &loExceptionInfo, NULL, NULL);
+
+	CloseHandle(lhDumpFile);
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 void initMiniDump(){
-	printf("Hello, Mini Dump\n");
+	SetUnhandledExceptionFilter(MyUnhandledExceptionFilter);
 }
--- DumpTest/DumpTest.cpp
+++ DumpTest/DumpTest.cpp
@@ -3,11 +3,21 @@
 
 #include "stdafx.h"
 #include "minidump.h"
+#include <stdexcept>
+#include "guard.h"
+
+void createCrash(){
+	throw std::runtime_error("Create a crash");
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	initMiniDump();
-	_tsystem(_T("pause"));
+    RAII::make_ScopeGuard([](){
+        printf("hello world\n");
+    });
+
+    createCrash();
+
 	return 0;
 }
-
